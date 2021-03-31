e2d4780996ca05880c912ab63f576bfa93eb297a
JuRyun <wngus2991@gmail.com>
2021-03-31 11:08:37 +0900

    inner class for multiple buttons

--- app/src/main/java/com/example/btnwinnerclass/MainActivity.java
+++ app/src/main/java/com/example/btnwinnerclass/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.btnwinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button nButton1;
-    TextView nTextView1;
 
+    Button nButton1, nButton;
+    TextView nTextView1;
 
 
     @Override
@@ -17,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nButton1 = (Button) findViewById(R.id.button1);
+        nButton = findViewById(R.id.button);
+        nButton1 = findViewById(R.id.button1);
         nTextView1 = findViewById(R.id.textView1);
 
-        nButton1.setOnClickListener(new MyOnClickListener(this));
+        nButton1.setOnClickListener(new MyOnClickListener());
+        nButton.setOnClickListener(new MyOnClickListener());
+    }
 
+    private class MyOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button1: nTextView1.setText("You Clicked Button 1"); break;
+                case R.id.button: nTextView1.setText("You Clicked Button"); break;
+                default: break;
+            }
+        }
     }
 }
